1b57b09613a1c45f7f3b73eae4be3749e575f66c
vhawk19 <varunhawk19@gmail.com>
2018-11-04 21:04:31 +0530

    added evalute function
    @VoxEtMohita @pranavmodx  
    
    added the evaluate function to check whether a straight line is formed or not by the os or xs….
    but i didnt get time to chekc it up...so it might be filled with errors...

--- cpp/tic-tac-toe.cpp
+++ cpp/tic-tac-toe.cpp
@@ -12,6 +12,55 @@ void display_cell(int a){
         cout<<"  ";
     }
 }
+int check(){
+    for(int i=0;i<3;i++){
+        int sum=0;
+        for(int j=0j<3;j++){
+            sum+=board[i][j];
+        }
+        if(sum==3){
+            return  10;
+        }
+        else if(sum==-3){
+            return -10;
+        }
+    }
+    for(int i=0;i<3;i++){
+        int sum=0;
+        for(int j=0j<3;j++){
+            sum+=board[j][i];
+        }
+        if(sum==3){
+            return  10;
+        }
+        else if(sum==-3){
+            return -10;
+        }
+    }
+    int sum_0=0;
+    for(int i=0;i<3;i++){
+        sum_0+=board[i][i];
+    }
+    if(sum_0==3){
+        return +10;
+    }
+    else if(sum_0==-3){
+        return -10;
+    }
+    sum_0=0;
+    for(int i=0;i<3;i++){
+        sum_0+=board[i][2-i];
+    }
+
+    if(sum_0==3){
+        return +10;
+    }
+    else if(sum_0==-3){
+        return -10;
+    }
+
+    return 0;
+}
 void intialize(){
     for(int i=0;i<3;i++){
         for(int j=0;j<3;j++){
